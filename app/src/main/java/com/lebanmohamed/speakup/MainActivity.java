ac9a5fb73ca4edfa943f8a0244cba980b4cc1403
TheBigL <lmohamed1@studentmail.nait.ca>
2019-01-11 18:29:02 -0700

    Initial commit

---
+++ app/src/main/java/com/lebanmohamed/speakup/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lebanmohamed.speakup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+}
